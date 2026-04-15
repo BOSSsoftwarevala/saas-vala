dc4820e630b0ddce420792221e6035e65f7ad916
YourName <your@email.com>
2026-04-15 15:19:21 +0530

    config: set dev server port to 8082 to match working system

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     envPrefix: 'VITE_',
     server: {
       host: "::",
-      port: 8080,
+      port: 8082,
       hmr: {
         overlay: false,
       },
